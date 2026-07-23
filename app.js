a67c5dd8742d9bb5348a08b02dfb9b94fa062ab0
dmg1784-cloud <dmg1784@gmail.com>
2026-07-23 15:53:56 +0800

    Update app.js

--- app.js
+++ app.js
@@ -270,3 +270,23 @@ async function onScanSuccess(
   );
 
 }
+if ("serviceWorker" in navigator) {
+
+  window.addEventListener("load", () => {
+
+    navigator.serviceWorker
+      .register("./sw.js")
+      .then(() => {
+
+        console.log("✅ Service Worker Registered");
+
+      })
+      .catch(err => {
+
+        console.error(err);
+
+      });
+
+  });
+
+}
